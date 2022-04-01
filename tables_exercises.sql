3f26e67f29da9b98e70702bc7ea13b00cfe8dc06
Kayla Brock <kayla.marie.brock@gmail.com>
2022-04-01 11:29:32 -0500

    tables exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,19 @@
+Use the employees database. Write the SQL code necessary to do this.
+--Answer: Below
+Use employees;
+List all the tables in the database. Write the SQL code necessary to accomplish this.
+--Answer: Below 
+ SHOW TABLES;
+Explore the employees table. What different data types are present on this table?
+--Answer: 
+Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
+--Answer: dept_emp, employees, dept_manager, salaries
+Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+--Answer: titles, employees, dept_emp, dept_manager, departments
+Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+--Answer: birth_date, hire_date
+What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
+--Answer: They both are in the table dept_emp 
+
+
