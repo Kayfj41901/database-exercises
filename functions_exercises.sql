4e8c69e35b956c140a0c14b1b1fd860dd21df9c3
Kayla Brock <kayla.marie.brock@gmail.com>
2022-04-01 10:39:41 -0500

    Functions Exercises

---
+++ functions_exercises.sql
@@ -0,0 +1,48 @@
+2.Write a query to to find all employees whose last name starts and ends with 'E'.
+ Use concat()
+ to combine their first and last name together as a single column named full_name.
+--Answer: First name: Ramzi.Erde
+Use employees;
+Select CONCAT(first_name, '.', last_name) as full_name
+from employees
+Where last_name like ('E%') and last_name like ('%E'); 
+
+3.Convert the names produced in your last query to all uppercase.
+--Answer: First name: RAMZI ERDE
+Use employees;
+Select UPPER(CONCAT(first_name, '.', last_name)) as full_name
+from employees
+Where last_name like ('E%') and last_name like ('%E') 
+
+
+4. Find all employees hired in the 90s and born on Christmas. 
+Use datediff() function to find how many days they have
+ been working at the company (Hint: You will also need 
+ to use NOW() or CURDATE()),
+--Answer: First name: Erie Mang 10390
+
+Use employees;
+Select (datediff(NOW(), hire_date)), last_name, first_name 
+From employees
+Where (hire_date like ('%199%')) And (birth_date like ('%12-25%'));
+
+Find the smallest and largest current salary from the salaries table.
+--Answer: Smallest Salary:38623 Largest Salary: 158220
+Use employees;
+Select salary
+from salaries
+ORDER BY salary ASC;
+
+Use employees;
+Select salary
+from salaries
+ORDER BY salary DESC;
+
+Use your knowledge of built in SQL functions to generate a username for all of the 
+employees. A username should be all lowercase, and consist of the first character 
+of the employees first name, the first 4 characters of the employees last name, 
+an underscore, the month the employee was born, and the last two digits of the 
+year that they were born. 
+Use employees; 
+Select lower(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), ('_'), SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)))
+from employees;
